8d6c94ec9be9fad49f02a9a9bc5b010929c411b9
charlie <charlesbkruger@gmail.com>
2020-01-07 17:28:54 +0200

    added validation

--- phonebook-ui/src/app/entry-add/entry-add.component.ts
+++ phonebook-ui/src/app/entry-add/entry-add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { RestService } from "../rest.service";
 import { ActivatedRoute, Router } from "@angular/router";
+import { MatDialog, MatDialogConfig } from "@angular/material";
+import { ConfirmDialogComponent } from "../confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: "app-product-add",
@@ -8,6 +10,8 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./entry-add.component.scss"]
 })
 export class EntryAddComponent implements OnInit {
+  valid: boolean;
+
   @Input() entryData = {
     name: "",
     surname: "",
@@ -22,7 +26,8 @@ export class EntryAddComponent implements OnInit {
   constructor(
     public rest: RestService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit() {}
@@ -42,14 +47,63 @@ export class EntryAddComponent implements OnInit {
   }
 
   addEntry() {
-    console.log("data being saved:: " + this.entryData);
-    this.rest.addEntry(this.entryData).subscribe(
-      result => {
-        this.router.navigate(["/entries"]);
-      },
-      err => {
-        console.log(err);
+    this.validate();
+
+    if (!this.valid) {
+      alert("Please check that all fields have been entered");
+    } else {
+      console.log("data being saved:: " + this.entryData);
+      this.rest.addEntry(this.entryData).subscribe(
+        result => {
+          this.router.navigate(["/entries"]);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  showDeleteDialog(item) {
+    if (item.description === "" && item.content === "") {
+      this.removeNumber(item);
+    } else {
+      const dialogConfig = new MatDialogConfig();
+
+      dialogConfig.autoFocus = true;
+      dialogConfig.data = {
+        title: "Remove contact detail",
+        description: "Are you sure you want remove to this contact detail?"
+      };
+
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, dialogConfig);
+
+      dialogRef
+        .afterClosed()
+        .subscribe(data => this.handleDeleteDialogResult(data, item));
+    }
+  }
+
+  handleDeleteDialogResult(data, item) {
+    if (data != null) {
+      if (data === true) {
+        this.removeNumber(item);
       }
-    );
+    }
+  }
+
+  validate() {
+    this.valid = true;
+
+    if (!this.entryData.name.length) {
+      this.valid = false;
+    }
+
+    this.entryData.contactDetails.forEach(element => {
+      if (!element.content.length || !element.description.length) {
+        this.valid = false;
+        return;
+      }
+    });
   }
 }
--- phonebook-ui/src/app/entry-edit/entry-edit.component.ts
+++ phonebook-ui/src/app/entry-edit/entry-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { RestService } from "../rest.service";
 import { ActivatedRoute, Router } from "@angular/router";
+import { MatDialog, MatDialogConfig } from "@angular/material";
+import { ConfirmDialogComponent } from "../confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: "app-entry-edit",
@@ -8,6 +10,8 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./entry-edit.component.scss"]
 })
 export class EntryEditComponent implements OnInit {
+  valid: boolean;
+
   @Input() entryData = {
     name: "",
     surname: "",
@@ -22,31 +26,30 @@ export class EntryEditComponent implements OnInit {
   constructor(
     public rest: RestService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit() {
-    this.rest
-      .getEntry(this.route.snapshot.params.id)
-      .subscribe(
-        (data: {
-          name: "";
-          surname: "";
-          contactDetails: [
-            {
-              description: "";
-              content: "";
-            }
-          ];
-        }) => {
-          console.log(data);
-          this.entryData = data;
-        }
-      );
+    this.rest.getEntry(this.route.snapshot.params.id).subscribe(
+      (data: {
+        name: "";
+        surname: "";
+        contactDetails: [
+          {
+            description: "";
+            content: "";
+          }
+        ];
+      }) => {
+        console.log(data);
+        this.entryData = data;
+      }
+    );
   }
 
   addNumber() {
-    this.entryData.contactDetails.push({description: "", content: "" });
+    this.entryData.contactDetails.push({ description: "", content: "" });
   }
 
   removeNumber(item) {
@@ -57,14 +60,63 @@ export class EntryEditComponent implements OnInit {
   }
 
   updateEntry() {
-    console.log(this.entryData);
-    this.rest.updateEntry(this.entryData).subscribe(
-      result => {
-        this.router.navigate(["/entry-detail/" + result.phonebookEntryId]);
-      },
-      err => {
-        console.log(err);
+    this.validate();
+
+    if (!this.valid) {
+      alert("Please check that all fields have been entered");
+    } else {
+      console.log(this.entryData);
+      this.rest.updateEntry(this.entryData).subscribe(
+        result => {
+          this.router.navigate(["/entry-detail/" + result.phonebookEntryId]);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  showDeleteDialog(item) {
+    if (item.description === "" && item.content === "") {
+      this.removeNumber(item);
+    } else {
+      const dialogConfig = new MatDialogConfig();
+
+      dialogConfig.autoFocus = true;
+      dialogConfig.data = {
+        title: "Remove contact detail",
+        description: "Are you sure you want remove to this contact detail?"
+      };
+
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, dialogConfig);
+
+      dialogRef
+        .afterClosed()
+        .subscribe(data => this.handleDeleteDialogResult(data, item));
+    }
+  }
+
+  handleDeleteDialogResult(data, item) {
+    if (data != null) {
+      if (data === true) {
+        this.removeNumber(item);
       }
-    );
+    }
+  }
+
+  validate() {
+    this.valid = true;
+
+    if (!this.entryData.name.length) {
+      this.valid = false;
+    }
+
+    this.entryData.contactDetails.forEach(element => {
+      if (!element.content.length || !element.description.length) {
+        this.valid = false;
+        return;
+      }
+    });
   }
 }
